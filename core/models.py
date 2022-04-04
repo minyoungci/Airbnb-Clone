d9603434249b40ddf1a088a0d6c997f55cf42940
min <80457917+minyoungci@users.noreply.github.com>
2022-04-04 15:13:32 +0900

    #4 room

---
+++ core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
+
+    class Meta:
+        abstract = True
--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
+
+
+@admin.register(models.Room)
+class RoomAdmin(admin.ModelAdmin):
+
+    pass
+
 
 # Register your models here.
