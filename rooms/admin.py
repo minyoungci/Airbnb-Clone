20c6610149bb71047ac81311b987698fa6b6aa87
min <80457917+minyoungci@users.noreply.github.com>
2022-04-07 00:08:26 +0900

    #6 1/2

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,6 +15,24 @@ class RoomAdmin(admin.ModelAdmin):
 
     """Room Admin Definition"""
 
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = ("instant_book", "city", "country")
+
+    search_fields = ("=city", "^host__username")
+
     pass
 
 
