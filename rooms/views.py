d3abaa16b7c9b5773ecd62a4370cdfc3b147fc68
minyoungci <dbssus123@gmail.com>
2022-05-13 20:15:43 +0900

    #13

--- rooms/views.py
+++ rooms/views.py
@@ -28,16 +28,35 @@ def search(request):
     country = request.GET.get("country", "KR")
     room_type = int(request.GET.get("room_type", 0))
     room_types = models.RoomType.objects.all()
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    s_amenities = request.GET.get("amenities")
+    s_facilities = request.GET.get("facilities")
+    print(s_amenities, s_facilities)
     # 프론트엔드에 필요한 form 정보
     form = {
         "city": city,
         "s_room_type": room_type,
         "s_country": country,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
     }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
     # 데이터베이스에 저장되어있는 정보
     choices = {
         "countries": countries,
         "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
     }
-
     return render(request, "rooms/search.html", {**form, **choices})
