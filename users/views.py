b3df93c05455869d1823e6927c6bcd2eef9eeb87
minyoungci <dbssus123@gmail.com>
2022-05-19 11:30:42 +0900

    model form

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import FormView
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import redirect, reverse
 from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login, logout
 from . import forms
@@ -23,3 +23,22 @@ class LoginView(FormView):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignupView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+
+    initial = {"first_name": "Min", "last_name": "See", "email": "dbssus123@gmail.com"}
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+
+        return super().form_valid(form)
